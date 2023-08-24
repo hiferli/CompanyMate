19129e3ac9f4a171147b0e2da75f25f10f0906b9
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-08-24 19:40:16 +0530

    Connecting to Database

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,15 +1,27 @@
 import express from 'express'
 import dotenv from "dotenv";
+import mongoose from 'mongoose'
 
 dotenv.config()
 
-const app = express()
-const port = process.env.BACKEND_PORT
+const app = express();
+
+const DATABASE_URI = process.env.MONGO_URI;
+
+mongoose.connect(DATABASE_URI);
+const db = mongoose.connection;
+db.on('error' , (error) => console.error(error));
+db.on('open' , () => console.log('Connection to database successful'));
+
+
+app.use(express.json())
+
 
 app.get('/', (request, response) => {
     response.send('Hello World!')
 })
 
+const port = process.env.BACKEND_PORT
 app.listen(port, () => {
     console.log(`Careers listening on port ${port}`)
 })
