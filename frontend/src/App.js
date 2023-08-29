20703dbd9b4750af52c695989f5691d05786db7c
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-08-30 00:04:10 +0530

    Creating Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Companies from './Pages/Companies';
 import UploadCompany from './Pages/UploadCompany';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
 	return (
-		<>
-			<Companies />
-			{/* <UploadCompany /> */}
-		</>
+		<Router>
+			<div>
+				<Routes>
+					<Route path="/upload-company" element={<UploadCompany />} />
+					<Route path="/companies" element={<Companies />} />
+				</Routes>
+			</div>
+		</Router>
+
 	);
 }
 
